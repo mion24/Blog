ad77f39714dac89c6a1640af00bc50f14e4b8b6d
mion24 <guilherme.milhomemf@gmail.com>
2024-04-02 19:23:52 -0400

    finalizado api

--- Blog.Application/Contexts/PostContext/Entities/Post.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Blog.Application.Contexts.PostContext.Entities
-{
-    public class Post
-    {
-        public Guid Id { get; set; }
-        public string Title { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
-    }
-}
--- Blog.Application/Contexts/ViewModels/AccountViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Blog.Application.Contexts.ViewModels
-{
-    public class AccountViewModel
-    {
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
